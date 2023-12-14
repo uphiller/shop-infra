369a5ffb0608c24bf2359de077c577fc8c0be666
youngho <jason.uphiller@gmail.com>
2023-12-14 13:19:50 +0900

    modify shell file

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -12,4 +12,4 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ../aws/install
+sudo ../../aws/install
