4b9abfd26c097d0161e1e47666787036f754bd33
youngho <jason.uphiller@gmail.com>
2023-12-18 15:45:58 +0900

    modify file

--- k8s/k8s.tf
+++ k8s/k8s.tf
@@ -4,7 +4,7 @@ resource "aws_lightsail_instance" "k8s-master" {
   blueprint_id      = "ubuntu_22_04"
   bundle_id         = "small_3_0"
   key_pair_name     = "shop"
-  user_data = "cd /home/ubuntu && sudo git clone https://github.com/uphiller/shop-infra.git && chmod 777 ./shop-infra/k8s/k8s.sh"
+  user_data = "cd /home/ubuntu && sudo git clone https://github.com/uphiller/shop-infra.git && chmod 777 ./shop-infra/k8s/master/*.sh"
 }
 
 resource "aws_lightsail_instance_public_ports" "k8s-master" {
@@ -45,6 +45,12 @@ resource "aws_lightsail_instance_public_ports" "k8s-master" {
     from_port = 10259
     to_port   = 10259
   }
+
+  port_info {
+    protocol  = "tcp"
+    from_port = 6443
+    to_port   = 6443
+  }
 }
 
 resource "aws_lightsail_instance" "k8s-node01" {
@@ -53,7 +59,7 @@ resource "aws_lightsail_instance" "k8s-node01" {
   blueprint_id      = "ubuntu_22_04"
   bundle_id         = "small_3_0"
   key_pair_name     = "shop"
-  user_data = "cd /home/ubuntu && sudo git clone https://github.com/uphiller/shop-infra.git && chmod 777 ./shop-infra/k8s/k8s.sh"
+  user_data = "cd /home/ubuntu && sudo git clone https://github.com/uphiller/shop-infra.git && chmod 777 ./shop-infra/k8s/node/*.sh"
 }
 
 resource "aws_lightsail_instance_public_ports" "k8s-node" {
