c9b350e2d05bbd001b40958af8f3dd08f8eec0f6
youngho <jason.uphiller@gmail.com>
2023-12-26 11:01:45 +0900

    add git-ops file

---
+++ k8s/shop/shop.sh
@@ -0,0 +1,19 @@
+kubectl create namespace argocd
+
+- appProtocol: http
+    name: mysql-svc
+    nodePort: 32281
+    port: 3306
+    protocol: TCP
+    targetPort: 3306
+
+kubectl edit deploy ingress-nginx-controller -n ingress-nginx
+- --tcp-services-configmap=ingress-nginx/tcp-services
+
+kubectl edit svc ingress-nginx-controller -n ingress-nginx
+- appProtocol: http
+    name: mysql-svc
+    nodePort: 32281
+    port: 3306
+    protocol: TCP
+    targetPort: 3306
