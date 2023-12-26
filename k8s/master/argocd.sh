16a0b4ae8629e2aacabf2cfac503957e40288a72
youngho <jason.uphiller@gmail.com>
2023-12-26 17:07:15 +0900

    add script

--- k8s/master/argocd.sh
+++ k8s/master/argocd.sh
@@ -1,4 +1,4 @@
 kubectl create namespace argocd
-kubectl create namespace data
+kubectl create namespace shop
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
--- k8s/shop/shop.sh
+++ k8s/shop/shop.sh
@@ -1,12 +1,3 @@
-kubectl create namespace argocd
-
-- appProtocol: http
-    name: mysql-svc
-    nodePort: 32281
-    port: 3306
-    protocol: TCP
-    targetPort: 3306
-
 kubectl edit deploy ingress-nginx-controller -n ingress-nginx
 - --tcp-services-configmap=ingress-nginx/tcp-services
 
